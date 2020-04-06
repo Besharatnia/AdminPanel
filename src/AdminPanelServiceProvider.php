362ce8936e21df0c460c61a0b8e4ac7c8f09e176
Alireza <Ali>
2020-04-06 13:09:25 +0430

    fix

--- src/AdminPanelServiceProvider.php
+++ src/AdminPanelServiceProvider.php
@@ -8,15 +8,21 @@ class AdminPanelServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/assets' => public_path('vendor/AdminPanel'),
-        ], 'public');
+//        $this->publishes([
+//            __DIR__.'/assets' => public_path('vendor/AdminPanel'),
+//        ], 'public');
 
 
-        $this->loadViewsFrom(__DIR__.'/views', 'AdminPanel');
+        $this->loadViewsFrom(__DIR__ . '/views', 'ap');
+
         $this->publishes([
-            __DIR__.'/views' => resource_path('views/vendor/AdminPanel'),
-        ]);
+//            __DIR__ . DIRECTORY_SEPARATOR . 'Config' . DIRECTORY_SEPARATOR . 'quickadmin.php'                                                  => config_path('quickadmin.php'),
+            __DIR__ . '/assets/plugins' => base_path('public/quickadmin'),
+        ], 'ap');
+
+//        $this->publishes([
+//            __DIR__.'/views' => resource_path('views/vendor/AdminPanel'),
+//        ]);
     }
 
     public function register()
